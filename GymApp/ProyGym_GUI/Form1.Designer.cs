097921a4df515d903dc369aa014f0cdd100f091a
Kerwin <joelss4029@gmail.com>
2021-05-24 01:48:24 -0500

    ultimo cambio

--- GymApp/ProyGym_GUI/Form1.Designer.cs
+++ GymApp/ProyGym_GUI/Form1.Designer.cs
@@ -29,6 +29,7 @@ namespace ProyGym_GUI
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.txtNombre = new System.Windows.Forms.Label();
@@ -38,8 +39,14 @@ namespace ProyGym_GUI
             this.renovarMatriculaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.lblSaludo = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lblSesion = new System.Windows.Forms.ToolStripStatusLabel();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBox1
@@ -102,18 +109,57 @@ namespace ProyGym_GUI
             // 
             this.lblSaludo.AutoSize = true;
             this.lblSaludo.BackColor = System.Drawing.Color.White;
-            this.lblSaludo.Font = new System.Drawing.Font("Microsoft YaHei", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSaludo.Font = new System.Drawing.Font("Microsoft YaHei", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblSaludo.Location = new System.Drawing.Point(18, 57);
             this.lblSaludo.Name = "lblSaludo";
-            this.lblSaludo.Size = new System.Drawing.Size(531, 46);
+            this.lblSaludo.Size = new System.Drawing.Size(417, 36);
             this.lblSaludo.TabIndex = 4;
             this.lblSaludo.Text = "Bienvenido : Usuario del gym";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2,
+            this.lblSesion});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 5;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(99, 17);
+            this.toolStripStatusLabel1.Text = "Matricula Activa :";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(0, 17);
+            this.toolStripStatusLabel2.Click += new System.EventHandler(this.toolStripStatusLabel2_Click);
+            // 
+            // lblSesion
+            // 
+            this.lblSesion.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.lblSesion.ForeColor = System.Drawing.Color.Cornsilk;
+            this.lblSesion.Name = "lblSesion";
+            this.lblSesion.Size = new System.Drawing.Size(50, 17);
+            this.lblSesion.Text = "Sesion : ";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.lblSaludo);
             this.Controls.Add(this.txtNombre);
             this.Controls.Add(this.pictureBox1);
@@ -127,6 +173,8 @@ namespace ProyGym_GUI
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -141,6 +189,11 @@ namespace ProyGym_GUI
         private System.Windows.Forms.ToolStripMenuItem renovarMatriculaToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
         private System.Windows.Forms.Label lblSaludo;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripStatusLabel lblSesion;
     }
 }
 
--- GymApp/ProyGym_GUI/Form1.cs
+++ GymApp/ProyGym_GUI/Form1.cs
@@ -13,6 +13,7 @@ namespace ProyGym_GUI
 {
     public partial class Form1 : Form
     {
+        TimeSpan horaentrada = new TimeSpan();
         public Form1()
         {
             InitializeComponent();
@@ -25,7 +26,22 @@ namespace ProyGym_GUI
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            lblSaludo.Text = $"csm : {clsCredenciales.Nombre}";
+            lblSaludo.Text = $"Bienvenido: {clsCredenciales.Nombre} {clsCredenciales.Apellido}";
+            horaentrada = DateTime.Now.TimeOfDay;
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.Text = "Super Gym da la hora" + DateTime.Now;
+
+            lblSesion.Text = "Tiempo de sesion:" +
+                DateTime.Now.TimeOfDay.Subtract(horaentrada).ToString().Substring(0, 8);
+        }
+
+        private void toolStripStatusLabel2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
